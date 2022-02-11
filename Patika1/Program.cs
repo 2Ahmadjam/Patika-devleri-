4d70e914e208da8d16b9d01146ce556a439e6f5e
AHMAD BADR EDDIN JAMOUS <98274758+2Ahmadjam@users.noreply.github.com>
2022-02-11 10:03:38 +0300

    Add files via upload

---
+++ Patika1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("isminize giriniz :");
+            String Ad = Console.ReadLine();
+            Console.WriteLine("SoyAdınıze Giriniz :");
+            String SoyAd = Console.ReadLine();
+            Console.WriteLine("Merhaba" + " " + Ad + "" + SoyAd);
+        }
+    }
+}
