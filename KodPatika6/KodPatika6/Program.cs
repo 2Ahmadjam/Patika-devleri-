8c192446157555da887b1d0140930591e3b766eb
AHMAD BADR EDDIN JAMOUS <98274758+2Ahmadjam@users.noreply.github.com>
2022-02-28 16:59:24 +0300

    Add files via upload

---
+++ KodPatika6/KodPatika6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace KodPatika6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Time = DateTime.Now.Hour;
+            if (Time >= 6 && Time <= 11)
+                Console.WriteLine("Günaydın");
+            else if (Time <= 18)
+
+                Console.WriteLine("İyi Günler");
+            else
+                Console.WriteLine("İyi Akşamlar");
+
+            String sonuc = Time <= 18 ? "İyi Günler" : "İyi Akşamlar";
+
+            sonuc = Time >= 6 && Time <= 12 ? "Günaydın!" : Time >= 12 && Time <= 18 ? "İyi Günler!" : Time > 18 && Time <= 10 ? "İyi Akşamlar!" : "İyi Geceler!"; 
+
+            Console.WriteLine(sonuc);
+            
+
+        }
+    }
+}
